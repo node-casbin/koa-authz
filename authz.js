4d6d51c65e83c286e0c6f8d6ea47c59425386f13
Chalin-Shi <18875906195@163.com>
2018-08-10 23:59:23 +0800

    :sparkles: add main js file for koa-authz middleware

---
+++ authz.js
@@ -0,0 +1,49 @@
+// Copyright 2014 Manu Martinez-Almeida.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+const { Enforcer } = require('casbin')
+
+// authz returns the authorizer, uses a Casbin enforcer as input
+module.exports = function authz (enforcer) {
+  return async (ctx, next) => {
+    try {
+			if (!(enforcer instanceof Enforcer)) {
+				throw new Error('Invalid enforcer')
+			}
+      const authzorizer = new BasicAuthorizer(ctx, enforcer)
+      if (!authzorizer.checkPermission()) {
+				ctx.status = 403
+			}
+      await next()
+    } catch (e) {
+      throw e
+    }
+  }
+}
+
+// BasicAuthorizer class stores the casbin handler
+class BasicAuthorizer {
+	constructor(ctx, enforcer) {
+		this.ctx = ctx
+		this.enforcer = enforcer
+	}
+
+	// getUserName gets the user name from the request.
+	// Currently, only HTTP basic authentication is supported
+	getUserName() {
+		// customize to get username from context
+		const {user} = this.ctx
+		const {username} = user
+		return username
+	}
+
+	// checkPermission checks the user/method/path combination from the request.
+	// Returns true (permission granted) or false (permission forbidden)
+	checkPermission() {
+		const {ctx, enforcer} = this
+		const {originalUrl: path, method} = ctx
+		const user = this.getUserName()
+		return enforcer.Enforce(user, path, method)
+	}
+}
