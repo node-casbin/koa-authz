1821def0bc3212196c62a7f415693a2d56f6d30d
Chalin-Shi <18875906195@163.com>
2018-08-13 18:05:08 +0800

    :rocket: complete koa-authz main test

--- test/server.js
+++ test/server.js
@@ -2,26 +2,23 @@ const Koa = require('koa');
 const { Enforcer } = require('casbin')
 const authz  = require('../authz')
 
-console.log('Starting koa-authz test server on http://localhost:3000/')
-console.log('')
-console.log('You can test the server by issuing curl commands like the following:')
-console.log('')
-console.log('curl http://localhost:3000/dataset1/resource1?username=alice  # should succeed (return 200 and {status: true})')
-console.log('curl http://localhost:3000/dataset1/resource1?username=chalin # should fail (return 403)')
-console.log('')
+const app = new Koa()
 
-const app = new Koa();
-
+// set userinfo
 app.use(async(ctx, next) => {
-  const {username} = ctx.query
-  ctx.user = {username}
-  await next()
-  ctx.body = {status: true}
+  try {
+    const username = ctx.get('Authorization') || 'anonymous'
+    ctx.user = {username}
+    await next()
+    ctx.body = {status: true}
+  } catch (e) {
+    ctx.status = 503
+  }
 })
-
+// use authz middleware
 app.use(authz(async() => {
-  const enforcer = await Enforcer.newEnforcer('../authz_model.conf', '../authz_policy.csv')
+  const enforcer = await Enforcer.newEnforcer('authz_model.conf', 'authz_policy.csv')
   return enforcer
 }))
 
-app.listen(3000)
+module.exports = app
