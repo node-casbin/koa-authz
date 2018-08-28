498fa442220a190eb1695a5d8582ac329635f42d
风狗 <909275705@qq.com>
2018-08-28 14:27:02 +0800

    add customer BasicAuthorizer options

--- authz.js
+++ authz.js
@@ -3,17 +3,20 @@
 // license that can be found in the LICENSE file.
 
 const { Enforcer } = require('casbin')
-
+const BasicAuthorizer = require('./BasicAuthorizer')
 // authz returns the authorizer, uses a Casbin enforcer as input
 module.exports = function authz (options) {
   return async (ctx, next) => {
     try {
-      const {newEnforcer} = options
+      const {newEnforcer, basicAuthorizer} = options
       const enforcer = await newEnforcer()
       if (!(enforcer instanceof Enforcer)) {
         throw new Error('Invalid enforcer')
       }
-      const authzorizer = new BasicAuthorizer(ctx, enforcer)
+      if (!(basicAuthorizer instanceof BasicAuthorizer)) {
+        throw new Error('Plase extends BasicAuthorizer class')
+      }
+      const authzorizer = basicAuthorizer || new BasicAuthorizer(ctx, enforcer)
       if (!authzorizer.checkPermission()) {
         ctx.status = 403
       }
@@ -21,31 +24,5 @@ module.exports = function authz (options) {
     } catch (e) {
       throw e
     }
-  }
-}
-
-// BasicAuthorizer class stores the casbin handler
-class BasicAuthorizer {
-  constructor (ctx, enforcer) {
-    this.ctx = ctx
-    this.enforcer = enforcer
-  }
-
-  // getUserName gets the user name from the request.
-  // Currently, only HTTP basic authentication is supported
-  getUserName () {
-    // customize to get username from context
-    const {user} = this.ctx
-    const {username} = user
-    return username
-  }
-
-  // checkPermission checks the user/method/path combination from the request.
-  // Returns true (permission granted) or false (permission forbidden)
-  checkPermission () {
-    const {ctx, enforcer} = this
-    const {originalUrl: path, method} = ctx
-    const user = this.getUserName()
-    return enforcer.enforce(user, path, method)
   }
 }
