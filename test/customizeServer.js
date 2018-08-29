b450b406d03e08cf33253bbfb5d37d411f5ad8d4
Yang Luo <hsluoyz@qq.com>
2018-08-29 22:09:23 +0800

    Add copyright header.

--- test/customizeServer.js
+++ test/customizeServer.js
@@ -1,13 +1,23 @@
-/**
- * @author Balalals
- * @date 2018-08-29
- * @Description:
- */
+// Copyright 2018 The Casbin Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 const Koa = require('koa')
 const { Enforcer } = require('casbin')
 const authz = require('../authz')
 const app = new Koa()
 const BasicAuthorizer = require('../BasicAuthorizer')
+
 // set userinfo
 app.use(async (ctx, next) => {
   try {
@@ -19,6 +29,7 @@ app.use(async (ctx, next) => {
     ctx.status = 503
   }
 })
+
 // use CustomizeAuthorizer
 class CustomizeAuthorizer extends BasicAuthorizer {
   // override function
@@ -33,6 +44,7 @@ class CustomizeAuthorizer extends BasicAuthorizer {
   //   return enforcer.enforce(user, path, method)
   // }
 }
+
 app.use(authz({
   newEnforcer: async () => {
     // load the casbin model and policy from files, database is also supported.
